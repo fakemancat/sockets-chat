852d301ba0848dc70a1d634f5c81a331f5dc792b
fakemancat <bryuhanovdima@yandex.ru>
2020-03-02 22:12:23 +0500

    Полный редизайн, обновлённый функционал, кнопка открытия чата

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const io = require('socket.io')();
 
+const MessageContext = require('./classes/MessageContext');
+
 io.on('connection', (socket) => {
     console.log('Client connected');
 
-    socket.on('message', async (data) => {
-        console.log('New message', data);
-        if (data === 'привет') {
-            socket.emit('message', 'Привет, как дела?');
+    socket.on('message', async(text) => {
+        const context = new MessageContext(socket, text);
+        context.resend();
+
+        if (context.text === 'привет') {
+            context.send('Ну привет, как дела?');
         }
     });
 });
